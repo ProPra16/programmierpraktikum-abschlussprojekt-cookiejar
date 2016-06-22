31ce12b83c2aa1c5b77aad4806aedb155dfbd68e
Michelle Werth <Michelle Werth>
2016-06-22 11:45:39 +0200

    delete unused imports, adjust insets, add text and textConsole styles

--- src/GUIDisplay.java
+++ src/GUIDisplay.java
@@ -11,7 +11,6 @@ import javafx.stage.Stage;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.PrintStream;
-import java.net.URL;
 
 public class GUIDisplay extends Application {
     public GUIControll controller;
