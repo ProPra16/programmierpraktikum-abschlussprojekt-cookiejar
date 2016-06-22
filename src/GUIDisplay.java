01939f4ac9d3a635678423c525c7667ea67ba411
Michelle Werth <Michelle Werth>
2016-06-22 12:17:31 +0200

    add color-change to GUI and modify stylesheets

--- src/GUIDisplay.java
+++ src/GUIDisplay.java
@@ -15,6 +15,7 @@ import java.io.PrintStream;
 public class GUIDisplay extends Application {
     public GUIControll controller;
     private Stage main;
+    private Scene scene;
     private int state;
 
     public GUIDisplay() {
@@ -30,7 +31,7 @@ public class GUIDisplay extends Application {
             Pane root = fxmlLoader.load(new FileInputStream("res/fxml/Cookiejar-main.fxml"));
             controller = (GUIControll) fxmlLoader.getController();
 
-            Scene scene = new Scene(root, 1280, 800);
+            scene = new Scene(root, 1280, 800);
 
             File f = new File("res/css/teststyle.css");
             scene.getStylesheets().add(f.toURI().toString());
@@ -42,7 +43,8 @@ public class GUIDisplay extends Application {
             //Add EventHandler for Cycle-button
             Button cycle = controller.getElementById("buttonCycle");
             cycle.addEventHandler(MouseEvent.MOUSE_CLICKED, (MouseEvent event) -> {
-                setState(state ^ 1);
+                state++;
+                setState(state);
             });
 
             //Redirect standart output to "console" TextArea
@@ -71,6 +73,9 @@ public class GUIDisplay extends Application {
                 tests.setEditable(true);
                 console.setEditable(false);
 
+                File f = new File("res/css/teststyle.css"); //set style to teststyle
+                scene.getStylesheets().add(f.toURI().toString());
+
                 state = pState;
                 console.appendText("You are now in test-writing mode\n");
             }
@@ -83,9 +88,26 @@ public class GUIDisplay extends Application {
                 tests.setEditable(false);
                 console.setEditable(false);
 
-                state = pState;
+                File f = new File("res/css/codestyle.css"); //set style to codestyle
+                scene.getStylesheets().add(f.toURI().toString());
 
+                state = pState;
                 console.appendText("You are now in code-writing mode\n");
+            }
+        }
+        if (pState == 2) {    //enable writing code and tests
+
+            if (true) {  //add save and load for files; also if tests fail after refactoring, go back to test writing
+
+                code.setEditable(true);
+                tests.setEditable(true);
+                console.setEditable(false);
+
+                File f = new File("res/css/refactorstyle.css"); //set style to refactorstyle
+                scene.getStylesheets().add(f.toURI().toString());
+
+                state = -1;
+                console.appendText("You are now in refactoring mode\n");
             }
         }
     }
