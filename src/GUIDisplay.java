ba01768420d204fac1b5a8e2b151d7fb8e1d3805
matthiasmetzen <matthiasmetzen@web.de>
2016-06-29 21:49:03 +0200

    refactor button handlers

--- src/GUIDisplay.java
+++ src/GUIDisplay.java
@@ -29,7 +29,7 @@ public class GUIDisplay extends Application {
     public GUIDisplay() {
         main = new Stage();
         start(main);
-        setState(0);
+        setState(0, null);
     }
 
     public void start(Stage stage) {
@@ -60,36 +60,30 @@ public class GUIDisplay extends Application {
             //Add EventHandler for Cycle-button
             Button cycle = controller.getElementById("buttonCycle");
             cycle.addEventHandler(MouseEvent.MOUSE_CLICKED, (MouseEvent event) -> {
-                setState(state+1<3?state+1:0);
+                TestResult tr = getTestResult();
+                if(tr != null) {
+                    System.out.println("Number of failed tests: " + tr.getNumberOfFailedTests());
+                    System.out.println("Number of successful tests: " + tr.getNumberOfSuccessfulTests());
+                }
+                setState(state+1<3?state+1:0, tr);
             });
 
             Button test = controller.getElementById("buttonTest");
             test.addEventHandler(MouseEvent.MOUSE_CLICKED, (MouseEvent event) -> {
-                try {
-                    TabPane tabPane = controller.getElementById("tabPane");
-                    ArrayList<ClassStruct> classes = new ArrayList();
-                    for (Tab tab : tabPane.getTabs()) {
-                        classes.add(new ClassStruct(tab.getText(), ((TextArea) tab.getContent()).getText(), (boolean) tab.getUserData()));
-                    }
-                    ClassStruct[] clarr = new ClassStruct[0];
-                    TestResult tr = getTestResult(classes.toArray(clarr));
-                    if(tr != null) {
-                        System.out.println("Number of failed tests: " + tr.getNumberOfFailedTests());
-                        System.out.println("Number of successful tests: " + tr.getNumberOfSuccessfulTests());
-                    }
-
-                } catch(Exception e) {
-                    System.out.println("[GUID] Exception: " + e);
+                TestResult tr = getTestResult();
+                if(tr != null) {
+                    System.out.println("Number of failed tests: " + tr.getNumberOfFailedTests());
+                    System.out.println("Number of successful tests: " + tr.getNumberOfSuccessfulTests());
                 }
             });
 
             Button save = controller.getElementById("buttonSave");
             save.addEventHandler(MouseEvent.MOUSE_CLICKED, (MouseEvent event) -> {
-                TextArea textCode = controller.getElementById("textCode");
-                TextArea textTest = controller.getElementById("textTest");
-
-                controller.saveToFile("MyCode",textCode.getText(),"test123", false);
-                controller.saveToFile("MyTest",textTest.getText(),"test123", true);
+                try {
+                    for (Tab tab : ((TabPane)controller.getElementById("tabPane")).getTabs()) {
+                        //ToDo: put correct identifier
+                        controller.saveToFile(tab.getText(), ((TextArea)tab.getContent()).getText(), "test123", (boolean)tab.getUserData());
+                    }
 
                 /* load with:
                 textCode.setText(controller.loadFromFile("MyCode","test123", false));
@@ -103,82 +97,100 @@ public class GUIDisplay extends Application {
                     else
                         textCode.setText(c.getCode());
                 */
+                } catch(Exception e) {}
             });
+
+            TextArea textCode = controller.getElementById("textCode");
+            TextArea textTest = controller.getElementById("textTest");
+            textCode.setUserData(false);
+            textTest.setUserData(true);
+
 
             //Redirect standart output to "console" TextArea
             TextArea console = controller.getElementById("textConsole");
             Console outStr = new Console(console);
             PrintStream ps = new PrintStream(outStr, true);
             System.setOut(ps);
-            System.setErr(ps);
+            //System.setErr(ps);
+            console.setEditable(false);
 
         } catch (Exception e) {
             System.out.println("[GUID] Unhandled exception: " + e);
         }
     }
 
-    public void setState(int pState) {
+    public void setState(int pState, TestResult tres) {
 
         TextArea console = controller.getElementById("textConsole");
-        TextArea code = controller.getElementById("textCode");
-        TextArea tests = controller.getElementById("textTest");
 
-        TestResult tres = null;
         if (pState == 0) {    //enable writing tests
+            try{
+                if (tres == null || tres.getNumberOfFailedTests() == 0) {  //add check if all tests pass
 
-            if (tres == null || tres.getNumberOfFailedTests() == 0) {  //add check if all tests pass
+                    for (Tab t : ((TabPane) controller.getElementById("tabPane")).getTabs()) {
+                        if ((boolean) t.getUserData())
+                            ((TextArea) t.getContent()).setEditable(true);
+                        else
+                            ((TextArea) t.getContent()).setEditable(false);
+                    }
 
-                code.setEditable(false);
-                tests.setEditable(true);
-                console.setEditable(false);
-
-                scene.getStylesheets().set(0 ,styles.get(0));
-
-                state = pState;
-                console.appendText("You are now in test-writing mode\n");
-            }
+                    scene.getStylesheets().set(0, styles.get(0));
+                    state = pState;
+                    console.appendText("You are now in test-writing mode\n");
+                }
+            } catch(Exception e) {}
         }
         if (pState == 1 && tres != null) {    //enable writing code
+            try {
+                if (tres.getNumberOfFailedTests() == 1) {  //add check if EXACTLY one test fails
 
-            if (tres.getNumberOfFailedTests() == 1) {  //add check if EXACTLY one test fails
+                    for (Tab t : ((TabPane) controller.getElementById("tabPane")).getTabs()) {
+                        if ((boolean) t.getUserData())
+                            ((TextArea) t.getContent()).setEditable(false);
+                        else
+                            ((TextArea) t.getContent()).setEditable(true);
+                    }
 
-                code.setEditable(true);
-                tests.setEditable(false);
-                console.setEditable(false);
 
-                scene.getStylesheets().set(0, styles.get(1));
+                    scene.getStylesheets().set(0, styles.get(1));
 
-                state = pState;
-                console.appendText("You are now in code-writing mode\n");
-            }
-            else
-                System.out.println(tres.getNumberOfFailedTests() + " tests failed. Needs to be exactly 1");
+                    state = pState;
+                    console.appendText("You are now in code-writing mode\n");
+                } else
+                    System.out.println(tres.getNumberOfFailedTests() + " tests failed. Needs to be exactly 1");
+            } catch(Exception e) {}
         }
         if (pState == 2 && tres != null) {    //enable writing code and tests
+            try {
+                if (tres.getNumberOfFailedTests() == 0) {  //add save and load for files; also if tests fail after refactoring, go back to test writing
 
-            if (tres.getNumberOfFailedTests() == 0) {  //add save and load for files; also if tests fail after refactoring, go back to test writing
+                    for (Tab t : ((TabPane) controller.getElementById("tabPane")).getTabs()) {
+                        if ((boolean) t.getUserData())
+                            ((TextArea) t.getContent()).setEditable(false);
+                        else
+                            ((TextArea) t.getContent()).setEditable(true);
+                    }
 
-                code.setEditable(true);
-                tests.setEditable(true);
-                console.setEditable(false);
+                    scene.getStylesheets().set(0, styles.get(2));
 
-                scene.getStylesheets().set(0, styles.get(2));
-
-                state = 2;
-                console.appendText("You are now in refactoring mode\n");
-            }
-            else
-                System.out.println(tres.getNumberOfFailedTests() + " tests failed. Needs to be exactly 0");
+                    state = 2;
+                    console.appendText("You are now in refactoring mode\n");
+                } else
+                    System.out.println(tres.getNumberOfFailedTests() + " tests failed. Needs to be exactly 0");
+            }catch(Exception e) {}
         }
     }
 
-    public TestResult getTestResult(ClassStruct[] classes) {
+    public TestResult getTestResult() {
         try {
-            ArrayList<CompilationUnit> cu = new ArrayList();
-            for(ClassStruct c: classes)
-                cu.add(new CompilationUnit(c.getName(),c.getCode(),c.isTest()));
-            CompilationUnit[] cuarr = new CompilationUnit[0];
-            JavaStringCompiler cmp = CompilerFactory.getCompiler(cu.toArray(cuarr));
+			TabPane tabPane = controller.getElementById("tabPane");
+			ArrayList<CompilationUnit> cu = new ArrayList();
+			for (Tab tab : tabPane.getTabs()) {
+				cu.add(new CompilationUnit(tab.getText(), ((TextArea) tab.getContent()).getText(), (boolean) tab.getUserData()));
+			}
+			CompilationUnit[] cuarr = new CompilationUnit[0];
+			cuarr = cu.toArray(cuarr);
+            JavaStringCompiler cmp = CompilerFactory.getCompiler(cuarr);
 
             cmp.compileAndRunTests();
             CompilerResult cmpres = cmp.getCompilerResult();
@@ -193,9 +205,12 @@ public class GUIDisplay extends Application {
                 return null;
             }
         }
+        catch (NullPointerException nptr) {
+            System.out.println("Could not compile!");
+        }
         catch(Exception e){
             System.out.println("[GUID] Exception: " + e);
-            return null;
         }
+        return null;
     }
 }
