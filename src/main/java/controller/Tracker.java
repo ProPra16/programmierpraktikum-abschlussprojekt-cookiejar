1e1385789d0d6d607905d00556bba9de63d5d036
yasch107 <tendalor.alb@hotmail.de>
2016-07-20 16:52:08 +0200

    add prev and next button methods; GUI still not working yet

--- src/main/java/controller/Tracker.java
+++ src/main/java/controller/Tracker.java
@@ -3,8 +3,8 @@ package controller;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TabPane;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import models.CodeTab;
@@ -20,9 +20,13 @@ public class Tracker {
     @FXML private Button buttonPrev;
     @FXML private Button buttonNext;
     @FXML private TabPane logTabs;
+    @FXML private Label labelThisTime;
+    @FXML private Label labelAverageTime;
 
     private Timer timer;
     private List<TrackSave> saves;
+    private int currentLog = 0;
+    private String[] labelTexts = {"test-writing: ", "code-writing: ", "refactor: "};
 
     public Tracker(){
         saves = new ArrayList<>();
@@ -35,7 +39,7 @@ public class Tracker {
         timer.reset();
     }
 
-    public String averageTime(int state){
+    private String averageTime(int state){
         int time = 0;
         for(TrackSave trackSave: saves)
             if (trackSave.getState() == state){
@@ -58,15 +62,66 @@ public class Tracker {
             FXMLLoader fxmlLoader = new FXMLLoader();
             GridPane root = fxmlLoader.load(new FileInputStream("res/fxml/Tracker.fxml"));
 
-            Scene scene = new Scene(root, 200, 232);
+            Scene scene = new Scene(root, 200, 264);
             File file = new File("res/css/startstyle.css");
             scene.getStylesheets().add(file.toURI().toString());
+            file = new File("res/css/scenestyle.css");
+            scene.getStylesheets().add(file.toURI().toString());
+
+            if(!saves.isEmpty())
+                for(CodeTab saveTab: saves.get(0).getTabs()){
+                    Tab tab = new Tab();
+                    TextArea textArea = new TextArea();
+                    textArea.setText(saveTab.getCode());
+                    textArea.setEditable(false);
+                    tab.setContent(textArea);
+                    tab.setText(saveTab.getText());
+                    tab.setClosable(false);
+                    logTabs.getTabs().add(tab);
+                }
+
+            labelThisTime.setText("Time used for this refactor: "+saves.get(0).getTime());
+            labelAverageTime.setText("Average time used to refactor: "+averageTime(2));
 
             stage.setScene(scene);
             stage.setTitle("Tracker");
             stage.show();
+
+            addEventHandler();
         } catch (Exception e){
             System.out.println("[GUID] Unhandled exception: " + e);
         }
+    }
+
+    private void addEventHandler(){
+        buttonPrev.addEventHandler(MouseEvent.MOUSE_CLICKED, (MouseEvent event) -> {
+            if (currentLog!=0) {
+                currentLog--;
+                loadSelectedTabs();
+            }
+        });
+
+        buttonNext.addEventHandler(MouseEvent.MOUSE_CLICKED, (MouseEvent event) -> {
+            if(currentLog!=saves.size()-1){
+                currentLog++;
+                loadSelectedTabs();
+            }
+        });
+    }
+
+    private void loadSelectedTabs(){
+        logTabs.getTabs().clear();
+        for (CodeTab saveTab : saves.get(currentLog).getTabs()) {
+            Tab tab = new Tab();
+            TextArea textArea = new TextArea();
+            textArea.setText(saveTab.getCode());
+            textArea.setEditable(false);
+            tab.setContent(textArea);
+            tab.setText(saveTab.getText());
+            tab.setClosable(false);
+            logTabs.getTabs().add(tab);
+        }
+        labelThisTime.setText("Time used for this"+labelTexts[saves.get(currentLog).getState()]+""+saves.get(currentLog).getTime());
+        labelAverageTime.setText("Average time used for "+labelTexts[saves.get(currentLog).getState()]+""+averageTime(saves.get(currentLog).getState()));
     }
 }
